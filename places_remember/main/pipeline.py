d6093afc580290efeead55bb54d4c90995c22148
Mikhail Nikiforov <nikiforov.mikhail.v@gmail.com>
2023-01-10 11:37:45 +0700

    Tests and commentary
    Created tests using pytest library, covered all methods in views.py; added commentaries to views methods, pipeline.

--- places_remember/main/pipeline.py
+++ places_remember/main/pipeline.py
@@ -1,12 +1,8 @@
 from .models import UserProfile
 
+# Pipeline for getting profile picture from Google and saving it to UserProfile.
+
 def save_profile(backend, user, response, is_new=False, *args, **kwargs):
-    if is_new and backend.name == "facebook":
-        # The main part is how to get the profile picture URL and then do what you need to do
-        UserProfile.objects.filter(user=user).update(
-            imageUrl='https://graph.facebook.com/{0}/picture/?type=large&access_token={1}'.format(response['id'],
-                                                                                                  response[
-                                                                                                      'access_token']))
-    elif backend.name == 'google-oauth2':
+    if backend.name == 'google-oauth2':
         if response.get('picture'):
             UserProfile.objects.filter(user=user).update(imageUrl=response['picture'])
